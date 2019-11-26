6ccf8b46cea720603ed6b8514a68521f770f5a12
Fabian Dean <fabian@fabiandean.dev>
2019-11-26 13:01:14 -0800

    Added ClientInputManager to manage messages sent from Server

--- Client.java
+++ Client.java
@@ -1,19 +1,22 @@
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.Stack;
 
 public class Client {
 
-    public Client() {}
+    public Client() {
+    }
 
     public static void main(String[] args) throws Exception {
-        
+        Stack<Message> serverMessages = new Stack();
+
         // Connect to Server
         ObjectOutputStream out;
         ObjectInputStream in;
         Message message = new Message();
         Socket s = new Socket("localhost", 1124);
-        
+
         // Send initial login Object
         out = new ObjectOutputStream(s.getOutputStream());
         message.setLogIn(true);
@@ -21,21 +24,21 @@ public class Client {
         out.writeObject(message);
         out.flush();
         out.reset();
-        
+
         // Setup Input
         in = new ObjectInputStream(s.getInputStream());
-        
-        while(true) {
-            
-            Object temp = in.readObject();
-            if(temp != null) {
+        Thread t = new Thread(new ClientInputManager(s, serverMessages, in));
+        t.start();
 
-                Message m = (Message)temp;
+        while (!s.isClosed()) {
+
+            if (!serverMessages.isEmpty()) {
+
+                Message m = (Message) serverMessages.pop();
                 System.out.println("Received Message From: " + m.getUsername());
-                
+
             }
         }
-        
-        
+
     }
 }

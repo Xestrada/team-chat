e9fa5fa5190f0d3d25167fb5cf9d8b9726da66d6
Maximiliano Cruz <46981101+mcruz21@users.noreply.github.com>
2019-11-29 19:13:08 -0800

    calls push method
    pops the top of the priority queue and uses push() from server to send to all clients

--- MessageManager.java
+++ MessageManager.java
@@ -2,31 +2,25 @@ import java.util.PriorityQueue;
 import java.util.ArrayList;
 
 public class MessageManager implements Runnable {
-    
+
     private volatile PriorityQueue q;
     private final ArrayList<Server> servers;
     private Message m;
-    
+
     public MessageManager(PriorityQueue q, ArrayList<Server> servers) {
         this.q = q;
         this.servers = servers;
     }
-    
-    @Override
-    public void run() {
-        
+    public  void run() {
         while(true) {
-
             if(!q.isEmpty()) {
-                // Pop message and add to Queue in all servers
-                m = (Message)q.poll();
+                m = (Message) q.poll();
                 servers.forEach(server -> {
-                    server.addMessage(m);
+                    server.push(m);
                 });
-   
             }
         }
-        
+
     }
-    
+
 }

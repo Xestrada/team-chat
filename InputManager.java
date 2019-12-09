8cead0bedbcc0b506497ee976054f216b84347e5
Jorge Estrada <Xestrada.Jorge@gmail.com>
2019-12-08 17:56:09 -0800

    Append current time in server

--- InputManager.java
+++ InputManager.java
@@ -1,14 +1,18 @@
 import java.io.IOException;
 import java.util.PriorityQueue;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class InputManager implements Runnable {
     
+    private DateTimeFormatter format;
     private volatile PriorityQueue<Message> q;
     private final Server client;
     
     public InputManager(PriorityQueue<Message> q, Server client) {
         this.q = q;
         this.client = client;
+        format = DateTimeFormatter.ofPattern("HH:mm");
     }
     
     @Override
@@ -27,6 +31,7 @@ public class InputManager implements Runnable {
                     if(m.isLoggingOut()) {
                         client.setToClose();
                     }
+                    m.setMessage(m.getMessage() + " " + format.format(LocalTime.now()));
                     q.add(m);
                 }
                 
